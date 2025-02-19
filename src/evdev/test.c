0a3e96791bfe08429435c83157452baea62401cb
Evilur <andrewflame@mail.ru>
2025-02-19 23:02:47 +0300

    Add an evdev test source code

---
+++ src/evdev/test.c
@@ -0,0 +1,31 @@
+#include "evdev.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(const int arg_c, const char* const arg_v[])
+{
+    /* Open the device for reading */
+    const char* const device_path = arg_v[1];
+    int device = open(device_path, O_RDONLY);
+
+    /* Infinite read the events from the device */
+    printf("Start listening to device input data...\n");
+    for (;;) {
+        /* Read the event 
+         * In case of an error, exit the loop */
+        struct input_event event;
+        if (read(device, &event, sizeof(event)) == -1) break;
+
+        /* Goto the next iteration if there is a garbage event */
+        if (event.code == 0) continue;
+
+        /* Print the current event */
+        printf("Code=%d Value=%d\n", event.code, event.value);
+    }
+
+    /* This code is executed only when 
+     * there is an error in reading the device */
+    printf("It looks like the input device has been disabled");
+    return 100;
+}
