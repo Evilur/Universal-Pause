2353b2136a5be71fc4715591ee6261d8dbcfa87e
Evilur <andrewflame@mail.ru>
2025-03-01 03:25:13 +0300

    Fixed an error when trying to read an unreadable file

--- src/script/evdev-test.sh
+++ src/script/evdev-test.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env sh
 
+# Read the event device using the event-test C binary
+# $1: the event device path
+read_device() {
+    # If we can read this device
+    if [[ -r $1 ]]; then
+        # Run the event device test for printing the event data
+        evdev-test $1
+
+        # Exit with the success code
+        exit 0
+    else  # If we cant read the device
+        echo "WE CAN'T READ THE DEVICE !!! (USE LOCALE THERE)"
+        exit 122
+    fi
+}
+
 # Include locale files
 source $ROOT_DIR/locale/evdev-test/$LOCALE
 
 # If we have an argument
 if [[ $# > 0 ]]; then
-    # Try reading the event data
-    evdev-test $1
-
-    # Exit with the success code
-    exit 0
+    # Read the event device
+    read_device $1
 fi
+
+# Define a counter for all event devices
+event_devices=0
 
 # Define a counter for available devices
 available_devices=0
@@ -29,9 +45,15 @@ if [[ -d /sys/class/input ]]; then
         device_path=$(tail -n 1 $input/event*/uevent)
         device_path=/dev/${device_path:8}
 
+        # Increase the number of event devices
+        let event_devices+=1
+
         # Can we read the device?
         if [[ -r $device_path ]]; then
+            # Increase the number of available devices
             let available_devices+=1
+
+            # Print the data
             echo -e "$device_path\t$device_name"
         fi
     done
@@ -40,7 +62,7 @@ fi
 # If we have available devices
 if [[ $available_devices > 0 ]]; then
     # Now user is selecting the device
-    echo -n "$SELECT_DEVICE [0-$(($available_devices - 1))]: "
+    echo -n "$SELECT_DEVICE [0-$(($event_devices - 1))]: "
     read device_event_number
 
     # Print the empty line
@@ -48,12 +70,9 @@ if [[ $available_devices > 0 ]]; then
 
     # If user has entered the correct number
     if [[ $device_event_number =~ ^[0-9]+$ ]] &&  # Check for a non-negative num
-       [[ $device_event_number -lt $available_devices ]]; then
-        # Run the event device test for printing the event data
-        evdev-test /dev/input/event$device_event_number
-
-        # Exit with the success code
-        exit 0
+       [[ $device_event_number -lt $event_devices ]]; then
+           # Read the event device
+           read_device /dev/input/event$device_event_number
     else # If user has entered the incorrect number
         echo $INCORRECT_INPUT
         exit 121
