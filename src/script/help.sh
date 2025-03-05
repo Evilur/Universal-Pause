24ad9bc195595b548d14031d299dceee1d8acec7
Evilur <andrewflame@mail.ru>
2025-03-05 14:58:21 +0300

    Update the help output

--- src/script/help.sh
+++ src/script/help.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
-# Read the help message
+# Print the help message
 cat $ROOT_DIR/locale/help/$LOCALE
+
+# Print examples
+echo '    universal-pause --help
+    universal-pause --run
+    universal-pause --run --volume 0.5 --silent
+    universal-pause --evfind
+    universal-pause --evtest
+    universal-pause --evtest /dev/input/event5'
+
 exit 0
