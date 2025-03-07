87106f089f4946aca3986d430208e30418737abf
Evilur <andrewflame@mail.ru>
2025-03-08 00:18:06 +0300

    Change the examples output

--- src/script/help.sh
+++ src/script/help.sh
@@ -4,11 +4,13 @@
 cat $SHAREDIR/locale/help/$LOCALE
 
 # Print examples
-echo '    universal-pause --help
+cat << EOF
+    universal-pause --help
     universal-pause --run
     universal-pause --run --volume 0.5 --silent
     universal-pause --evfind
     universal-pause --evtest
-    universal-pause --evtest /dev/input/event5'
+    universal-pause --evtest /dev/input/event5
+EOF
 
 exit 0
